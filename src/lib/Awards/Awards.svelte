c309b40622e07f016185819b459fdfee4d63316e
xnickedge <83649070+xnickedge@users.noreply.github.com>
2021-08-02 11:39:22 -0400

    Update Awards.svelte

--- src/lib/Awards/Awards.svelte
+++ src/lib/Awards/Awards.svelte
@@ -1,9 +1,7 @@
 <script>
 	import {cleanName} from '$lib/utils/helper';
 	export let podium, currentManagers;
-
 	const { year, champion, second, third, divisions, toilet } = podium;
-
 	const getNames = (name, rosterID) => {
 		if(cleanName(name) != cleanName(currentManagers[rosterID].name)) {
 			return `${name}<div class="curOwner">(${currentManagers[rosterID].name})</div>`;
@@ -16,25 +14,21 @@
 	* {
 		color: #777;
 	}
-
 	h3 {
 		margin: 2.5em 0 1.5em;
 	}
-
 	.awards {
 		display: block;
 		position: relative;
 		width: 100%;
 		z-index: 1;
 	}
-
 	#podium {
 		width: 600px;
 		height: 500px;
 		position: relative;
 		margin: 10px auto 30px;
 	}
-
 	.podiumImage {
 		position: absolute;
 		bottom: 0;
@@ -43,7 +37,6 @@
 		height: auto;
 		z-index: 3;
 	}
-
 	.champ {
 		position: absolute;
 		width: 20%;
@@ -53,7 +46,6 @@
 		border: 1px solid #bbb;
 		background-color: #fff;
 	}
-
 	.laurel {
 		position: absolute;
 		width: 33%;
@@ -62,38 +54,31 @@
 		bottom: 56.6%;
 		left: 50%;
 	}
-
 	.first {
 		bottom: 70%;
 		left: 50%;
 	}
-
 	.second {
 		bottom: 43%;
 		left: 20%;
 	}
-
 	.third {
 		bottom: 39%;
 		left: 80%;
 	}
-
 	h3 {
 		text-align: center;
 	}
-
 	.leaderBlock {
 		position: relative;
 		width: 80px;
 		height: 119px;
 		margin: 15px auto;
 	}
-
 	.divisions {
 		display: flex;
 		justify-content: space-around;
 	}
-
 	.divisionLeader {
 		position: absolute;
 		width: 70px;
@@ -105,7 +90,6 @@
 		border: 1px solid #bbb;
 		z-index: 3;
 	}
-
 	.medal {
 		position: absolute;
 		width: 40px;
@@ -115,14 +99,12 @@
 		left: 50%;
 		z-index: 2;
 	}
-
 	.toiletBowl {
 		position: relative;
 		width: 215px;
 		height: 190px;
 		margin: 10px auto;
 	}
-
 	.toiletWinner {
 		position: absolute;
 		width: 65px;
@@ -134,7 +116,6 @@
 		border: 1px solid #bbb;
 		z-index: 3;
 	}
-
 	.toilet {
 		position: absolute;
 		width: 100%;
@@ -143,7 +124,6 @@
 		bottom: 0;
 		left: 50%;
 	}
-
 	.label {
 		white-space: nowrap;
 		line-height: 1.1em;
@@ -159,22 +139,18 @@
 		border: 1px solid #bbb;
         box-shadow: 0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%);
 	}
-
 	.firstLabel {
 		bottom: 60%;
 		left: 50%;
 	}
-
 	.secondLabel {
 		bottom: 40%;
 		left: 20%;
 	}
-
 	.thirdLabel {
 		bottom: 36%;
 		left: 80%;
 	}
-
 	.genLabel {
 		white-space: nowrap;
 		line-height: 1.1em;
@@ -189,11 +165,9 @@
 		border: 1px solid #bbb;
 		box-shadow: 0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%);
 	}
-
 	.division {
 		text-align: center;
 	}
-
 	.toiletParent {
 		width: 100%;
 		text-align: center;
@@ -201,27 +175,23 @@
 		margin-top: 30px;
 		box-shadow: 0 12px 9px -12px rgba(0,0,0,0.4);
 	}
-
 	.banner {
 		display: block;
 		width: 65%;
 		max-width: 450px;
 		margin: 20px auto 0;
 	}
-
 	.toilet-banner {
 		display: block;
 		width: 50%;
 		max-width: 350px;
 		margin: 20px auto 0;
 	}
-
 	:global(.curOwner) {
 		font-size: 0.75em;
 		color: #bbb;
 		font-style: italic;
 	}
-
 	@media (max-width: 680px) {
 		.label {
 			padding: 6px 8px;
@@ -230,7 +200,6 @@
 			padding: 6px 8px;
 		}
 	}
-
 	@media (max-width: 630px) {
 		.label {
 			font-size: 0.9em;
@@ -239,7 +208,6 @@
 			font-size: 0.9em;
 		}
 	}
-
 	@media (max-width: 610px) {
 		#podium {
 			width: 500px;
@@ -247,20 +215,16 @@
 			position: relative;
 			margin: 10px auto 30px;
 		}
-
 		.firstLabel {
 			bottom: 58%;
 		}
-
 		.secondLabel {
 			bottom: 35%;
 		}
-
 		.thirdLabel {
 			bottom: 31%;
 		}
 	}
-
 	@media (max-width: 535px) {
 		.label {
 			font-size: 0.8em;
@@ -269,7 +233,6 @@
 			font-size: 0.8em;
 		}
 	}
-
 	@media (max-width: 520px) {
 		.label {
 			font-size: 0.7em;
@@ -280,14 +243,12 @@
 			padding: 2px 4px;
 		}
 	}
-
 	@media (max-width: 510px) {
 		#podium {
 			width: 400px;
 			height: 333px;
 		}
 	}
-
 	@media (max-width: 425px) {
 		.label {
 			font-size: 0.6em;
@@ -296,26 +257,21 @@
 			font-size: 0.6em;
 		}
 	}
-
 	@media (max-width: 410px) {
 		#podium {
 			width: 300px;
 			height: 250px;
 		}
-
 		.firstLabel {
 			bottom: 53%;
 		}
-
 		.secondLabel {
 			bottom: 31%;
 		}
-
 		.thirdLabel {
 			bottom: 27%;
 		}
 	}
-
 	@media (max-width: 329px) {
 		.label {
 			font-size: 0.5em;
@@ -347,18 +303,20 @@
 	</div>
 	<div class="divisions">
 		{#each divisions as division}
-			<div class="division">
-				{#if division.name}
-					<h6>{division.name} Division</h6>
-				{:else}
-					<h6>Regular Season Champion</h6>
-				{/if}
-				<div class="leaderBlock">
-					<img src="{division.manager.avatar}" class="divisionLeader" alt="{division.name} champion" />
-					<img src="./medal.png" class="medal" alt="champion" />
+			{#if division.manager}
+				<div class="division">
+					{#if division.name}
+						<h6>{division.name} Division</h6>
+					{:else}
+						<h6>Regular Season Champion</h6>
+					{/if}
+					<div class="leaderBlock">
+						<img src="{division.manager.avatar}" class="divisionLeader" alt="{division.name} champion" />
+						<img src="./medal.png" class="medal" alt="champion" />
+					</div>
+					<span class="genLabel">{@html getNames(division.manager.name, division.manager.rosterID)}</span>
 				</div>
-				<span class="genLabel">{@html getNames(division.manager.name, division.manager.rosterID)}</span>
-			</div>
+			{/if}
 		{/each}
 	</div>
 
